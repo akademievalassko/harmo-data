742937910caed84bfc3810be5550bce13442a0fd
akademievalassko <info@czpvk.cz>
2026-08-22 09:58:06 +0200

    Create events.js

---
+++ events.js
@@ -0,0 +1,126 @@
+harmoData([
+  {
+    "title": "Soustředění talentované mládeže (box, MMA)",
+    "date": "2026-01-31",
+    "cat": "kids",
+    "props": []
+  },
+  {
+    "title": "Výjezd karate - soustředění Kroměříž",
+    "date": "2026-02-24",
+    "cat": "kids",
+    "props": []
+  },
+  {
+    "title": "Soustředění talentované mládeže (box, MMA)",
+    "date": "2026-02-28",
+    "cat": "kids",
+    "props": []
+  },
+  {
+    "title": "Soustředění talentované mládeže (box, MMA)",
+    "date": "2026-03-01",
+    "cat": "kids",
+    "props": []
+  },
+  {
+    "title": "Výjezd karate - soustředění Hustopeče",
+    "date": "2026-03-07",
+    "cat": "kids",
+    "props": [
+      "https://ce97d0b040.clvaw-cdnwnd.com/63d00d6f221a2117a21962c3d9ff1f63/200002353-4a2f54a2f7/0.webp?ph=ce97d0b040"
+    ]
+  },
+  {
+    "title": "Soustředění karate - přespávačka",
+    "date": "2026-03-14",
+    "cat": "kids",
+    "props": []
+  },
+  {
+    "title": "Zkoušky STV karate",
+    "date": "2026-03-15",
+    "cat": "kids",
+    "props": []
+  },
+  {
+    "title": "Bruntálský Beginner Cup 2026 - eliminace",
+    "date": "2026-03-19",
+    "cat": "kids",
+    "props": [
+      "https://ce97d0b040.clvaw-cdnwnd.com/63d00d6f221a2117a21962c3d9ff1f63/200002355-135881358a/0-7.webp?ph=ce97d0b040"
+    ]
+  },
+  {
+    "title": "Valašská činka",
+    "date": "2026-03-28",
+    "cat": "kids",
+    "props": [
+      "https://ce97d0b040.clvaw-cdnwnd.com/63d00d6f221a2117a21962c3d9ff1f63/200002317-025a5025a6/Plak%C3%A1t%20830x1551%20JPEG.webp?ph=ce97d0b040"
+    ]
+  },
+  {
+    "title": "Kroměřížský Beginner Cup 2026",
+    "date": "2026-04-16",
+    "cat": "all",
+    "props": [
+      "https://ce97d0b040.clvaw-cdnwnd.com/63d00d6f221a2117a21962c3d9ff1f63/200002377-540b6540b8/Propozice.webp?ph=ce97d0b040"
+    ]
+  },
+  {
+    "title": "Fitbox pro veřejnost a workshop pro mládež s Vadime Zharovem",
+    "date": "2026-04-26",
+    "cat": "all",
+    "props": [
+      "https://ce97d0b040.clvaw-cdnwnd.com/63d00d6f221a2117a21962c3d9ff1f63/200002367-a9247a924a/WhatsApp%20Image%202026-04-07%20at%2012.45.59.webp?ph=ce97d0b040",
+      "https://ce97d0b040.clvaw-cdnwnd.com/63d00d6f221a2117a21962c3d9ff1f63/200002361-95d5f95d61/Sn%C3%ADmek%20obrazovky_31-3-2026_1540_www.czpvk.cz.webp?ph=ce97d0b040"
+    ]
+  },
+  {
+    "title": "VALAŠSKOKLOBOUCKÝ Beginner Cup 2026",
+    "date": "2026-04-30",
+    "cat": "all",
+    "props": [
+      "https://ce97d0b040.clvaw-cdnwnd.com/63d00d6f221a2117a21962c3d9ff1f63/200002381-a007aa007c/0-1.webp?ph=ce97d0b040"
+    ]
+  },
+  {
+    "title": "Přednáška Mgr. Petry Burešové SPORT DĚTÍ - SPORT A ŽENY - SPORT PO 50",
+    "date": "2026-04-21",
+    "cat": "all",
+    "props": [
+      "https://ce97d0b040.clvaw-cdnwnd.com/63d00d6f221a2117a21962c3d9ff1f63/200002369-5956c5956e/P%C5%99edn%C3%A1%C5%A1ka.webp?ph=ce97d0b040"
+    ]
+  },
+  {
+    "title": "Přednáška Mgr. Petry Burešové II. cyklus SPORT DĚTÍ - SPORT A ŽENY - SPORT PO 50",
+    "date": "2026-05-06",
+    "cat": "all",
+    "props": [
+      "https://ce97d0b040.clvaw-cdnwnd.com/63d00d6f221a2117a21962c3d9ff1f63/200002369-5956c5956e/P%C5%99edn%C3%A1%C5%A1ka.webp?ph=ce97d0b040"
+    ]
+  },
+  {
+    "title": "Karate Agility Challenge Vol. 4 Olomoucký Beginner Cup",
+    "date": "2026-05-14",
+    "cat": "all",
+    "props": [
+      "https://ce97d0b040.clvaw-cdnwnd.com/63d00d6f221a2117a21962c3d9ff1f63/200002385-0c6c80c6ca/Olomouck%C3%BD-Beginner-cup-2026-vol_4.webp?ph=ce97d0b040"
+    ]
+  },
+  {
+    "title": "GYMNASTICKÝ WORKSHOP pod vedením reprezentantky ČR Mgr. Niky Jáňové, šéftrenérky Movement Education",
+    "date": "2026-05-26",
+    "cat": "kids",
+    "props": []
+  },
+  {
+    "title": "Atletické závody dětí – otevřená soutěž",
+    "date": "2026-05-28",
+    "cat": "kids",
+    "props": [
+      "https://ce97d0b040.clvaw-cdnwnd.com/63d00d6f221a2117a21962c3d9ff1f63/200002373-95cd295cd4/Plak%C3%A1t%20AZ.webp?ph=ce97d0b040",
+      "https://ce97d0b040.clvaw-cdnwnd.com/63d00d6f221a2117a21962c3d9ff1f63/200002375-2a4f62a4f7/Propozice%20AZ.webp?ph=ce97d0b040"
+    ]
+  }
+]);
